5a9dc217b1a780e6d9fb8e3bea320e1670910e8d
Nuno9180580 <9180580@esmad.ipp.pt>
2022-06-13 18:28:17 +0100

    fixed api connection, created tables and admin user

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,7 @@ const UserController = require("../controllers/user.controller");
 const auth = require("../middleware/auth");
 
 router.get("/", auth, UserController.list);
-router.post("/register", UserController.register);
+// router.post("/register", UserController.register);
 router.post("/sign-in", UserController.signIn);
 
 module.exports = router;
